fb12db19c17c405c7e6f452e5d785663acab0280
Ashwini <ashiwinibodhkar@gmail.com>
2020-11-05 02:02:25 +0530

    font color change

--- gatsby-config.js
+++ gatsby-config.js
@@ -178,7 +178,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          `Fira Sans Condensed`
+          `Fira Sans Condensed`,`Bowlby One SC`,`Rancho`,`Yesteryear`,`Denk One`,`Fruktur`,`Zhi Mang Xing`,`Bree Serif`,`Caveat`,`Carter One`
            
         ],
         display: 'swap'
