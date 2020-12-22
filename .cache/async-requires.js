cfb456fc69b2e1968e5f7e2150a67da3bdd218d8
Ashwini <ashiwinibodhkar@gmail.com>
2020-12-23 01:05:02 +0530

    list change

---
+++ .cache/async-requires.js
@@ -0,0 +1,21 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-allartist-js": () => import("./../../../src/pages/allartist.js" /* webpackChunkName: "component---src-pages-allartist-js" */),
+  "component---src-pages-allsongs-js": () => import("./../../../src/pages/allsongs.js" /* webpackChunkName: "component---src-pages-allsongs-js" */),
+  "component---src-pages-artist-js": () => import("./../../../src/pages/artist.js" /* webpackChunkName: "component---src-pages-artist-js" */),
+  "component---src-pages-category-js": () => import("./../../../src/pages/category.js" /* webpackChunkName: "component---src-pages-category-js" */),
+  "component---src-pages-contact-js": () => import("./../../../src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-favourites-js": () => import("./../../../src/pages/favourites.js" /* webpackChunkName: "component---src-pages-favourites-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-login-js": () => import("./../../../src/pages/login.js" /* webpackChunkName: "component---src-pages-login-js" */),
+  "component---src-pages-signup-js": () => import("./../../../src/pages/signup.js" /* webpackChunkName: "component---src-pages-signup-js" */),
+  "component---src-pages-using-typescript-tsx": () => import("./../../../src/pages/using-typescript.tsx" /* webpackChunkName: "component---src-pages-using-typescript-tsx" */),
+  "component---src-template-artist-template-js": () => import("./../../../src/template/artist-template.js" /* webpackChunkName: "component---src-template-artist-template-js" */),
+  "component---src-template-category-template-js": () => import("./../../../src/template/category-template.js" /* webpackChunkName: "component---src-template-category-template-js" */),
+  "component---src-template-chord-js": () => import("./../../../src/template/chord.js" /* webpackChunkName: "component---src-template-chord-js" */)
+}
+
